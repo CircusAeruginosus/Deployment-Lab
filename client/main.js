62b114fb6929b184badfe20ea68e9e198295b2d2
Caitlin Thompson <ctt00297@gmail.com>
2021-12-01 15:02:15 -0600

    completed

---
+++ client/main.js
@@ -0,0 +1,7 @@
+let buyBtn = document.querySelector("button")
+
+let video = document.querySelector("#embed-video")
+
+buyBtn.addEventListener("click", () => {
+    video.style.display = "block"
+})
