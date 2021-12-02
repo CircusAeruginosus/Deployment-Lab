5d438f9b27b1dfac459a1dd0806ca948fc2526f6
Caitlin Thompson <ctt00297@gmail.com>
2021-12-02 14:24:15 -0600

    updated index.js

--- server/index.js
+++ server/index.js
@@ -1,14 +1,35 @@
-const express = require("express")
-const cors = require("cors")
-const path = require("path")
-
+const { Console } = require("console");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 const app = express()
+const Rollbar = require("rollbar");
 
 app.use(express.static("client"))
 
+const rollbar = new Rollbar({
+    accessToken: 'cb70f6fc48504b9b89581c679d3f4171',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+})
+
+//endpoints
 app.get("/", () => {
     resizeBy.sendFile(path.join(__dirname, "../index.html"))
 })
+
+app.get("/", (req, res) => {
+    try {
+        nonExistentFunction()
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+
+
+
+rollbar.log("server started")
 
 const port = process.env.PORT || 4040
 
